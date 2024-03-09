da358b26dad7b7a8c7a4c593affd04a48b22f775
robiulalamdev <robiulalam762400@gmail.com>
2024-03-09 12:11:19 +0600

    home page proggress

--- src/server/route/app.router.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter } from "../createRouter";
-import { categoryRouter } from "./category.router";
-import { userRouter } from "./user.router";
-
-export const appRouter = createRouter()
-  .merge("users.", userRouter)
-  .merge("categories.", categoryRouter);
-
-export type AppRouter = typeof appRouter;
--- src/utils/trpc.ts
+++
@@ -1,4 +0,0 @@
-import { createReactQueryHooks } from "@trpc/react";
-import { AppRouter } from "../server/route/app.router";
-
-export const trpc = createReactQueryHooks<AppRouter>();
